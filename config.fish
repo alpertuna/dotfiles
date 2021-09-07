f9b9c4ffc0110bf4e24c25891e7ae3935cc68d22
H.Alper TUNA <halpertuna@gmail.com>
2021-09-07 05:50:57 +0300

    First commit

---
+++ config.fish
@@ -0,0 +1,10 @@
+alias vim='nvim'
+
+fish_vi_key_bindings
+
+# set -g theme_display_vi no
+set -g theme_color_scheme gruvbox
+#set -g theme_powerline_fonts no
+#set -g theme_nerd_fonts yes
+set -g theme_show_exit_status yes
+set -g theme_display_jobs_verbose yes
